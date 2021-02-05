bf96df22c40c5850cd2ab91939c31e75e2e9515d
kylemartinolich <kylemartinolich@gmail.com>
2021-02-05 00:00:06 +0000

    test 4

---
+++ environment-website/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Button extends Component(){
+    render(){
+        var button = (<button>Hello</button>);
+        return button;
+    }
+};
+export default Button;
