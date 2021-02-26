4f388512a4402f4d4738c5ac61e6b0930847a8ed
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-02-26 20:08:36 +0000

    cleaned up the footer a bit with a button and some resizing of the forms. Added copyright at the bottom and uploaded an icon.svg from bootstrap for future use

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import classes from './Footer.module.css';
 const footer = (props) => (
     <div className={classes.Footer}>
         <h2><strong>Lorem Ipsum</strong></h2>
-        <Form className="justify-content-center">
+{/*       <Form className="justify-content-center">
     <Row>
         <Col sm={2}>
       <Form.Control placeholder="First name" />
@@ -21,8 +22,27 @@ const footer = (props) => (
     <Form.Control type="email" placeholder="Enter email" />
 
   </Col></Row>
-</Form>
+</Form> */}
 
+<Form>
+  <Form.Row className="justify-content-md-center">
+    <Col sm={2}>
+      <Form.Control placeholder="First name" />
+    </Col>
+    <Col sm={2}>
+      <Form.Control placeholder="Last name" />
+    </Col>
+    <Col sm={4}>
+        <Form.Control type="email" placeholder="Enter email" />
+    </Col>
+    <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  </Form.Row>
+</Form>
+<span>
+    © 2021 
+</span>
     </div>
 );
 
