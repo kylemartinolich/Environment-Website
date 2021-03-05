89d69e173ec28bc9783139b1ca543cf03d3861f0
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-03-05 18:30:49 +0000

    switched to svg icon instead of img

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -6,7 +6,6 @@ import Button from 'react-bootstrap/Button';
 import LandingButton from '../../components/LandingButton/LandingButton';
 import Chattahoochee_watershed from '../../assets/Chattahoochee_watershed.png';
 import classes from './LandingPage.module.css';
-import Image from 'react-bootstrap/Image';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
 
@@ -48,7 +47,10 @@ class LandingPage extends Component{
                                     delay={{ show: 250, hide: 400 }}
                                     overlay={renderTooltip}
                                 >
-                                   <Image src="././assets/Miscellaneous/info.png" fluid />
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                    </svg>
                                                                   
                                 </OverlayTrigger>,
                             </div>
