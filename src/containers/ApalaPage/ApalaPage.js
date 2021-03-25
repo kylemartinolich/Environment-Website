0f2abdb2f7d3f8a5f1b0e40c0b418ce2274f5dae
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-03-25 03:29:35 +0000

    added copyrights to all pages

--- src/containers/ApalaPage/ApalaPage.js
+++ src/containers/ApalaPage/ApalaPage.js
@@ -9,6 +9,14 @@ import AccordionPage from '../../components/Accordion/Accordion';
 import SpeciesData from '../../speciesdata.json';
 import AnimalButton from '../../components/AnimalButton/AnimalButton';
 import Plus from '../../assets/Miscellaneous/plus.png';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+const renderTooltip = (props) => (
+  <Tooltip id="button-tooltip" {...props}>
+     Apalachicola River, Georgia © Ebyabe
+  </Tooltip>
+);
 class ApalaPage extends Component{
     state={
         modalShow: false
@@ -29,7 +37,19 @@ class ApalaPage extends Component{
                     </div>
 
                 </div>
-
+            <div class="position-absolute bottom-0 left-0">
+                    <OverlayTrigger
+                        placement="right"
+                            delay={{ show: 250, hide: 400 }}
+                                overlay={renderTooltip}
+                                >
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                    </svg>
+                                                                  
+                    </OverlayTrigger>,
+                </div>
             </header>
             
             <AccordionPage animalList={SpeciesData.apa}/>
