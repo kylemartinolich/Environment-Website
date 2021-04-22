9486ab38908ecf315460baec623eb88cea09ebb9
Kdmman2 <kdmman2@gmail.com>
2021-04-22 19:52:54 +0000

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,10 @@ import Col from 'react-bootstrap/Col';
 //import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+import Toast from 'react-bootstrap/Toast';
 import classes from './Footer.module.css';
+
 const footer = (props) => (
     <div className={classes.Footer}>
         <h2><strong>Find out more!</strong></h2>
@@ -24,16 +27,16 @@ const footer = (props) => (
   </Col></Row>
 </Form> */}
 
-<Form>
+<Form onSubmit={sendEmail}>
   <Form.Row className="justify-content-md-center">
     <Col sm={2}>
-      <Form.Control placeholder="First name" />
+      <Form.Control placeholder="First name" name="first_name" />
     </Col>
     <Col sm={2}>
-      <Form.Control placeholder="Last name" />
+      <Form.Control placeholder="Last name" name="last_name" />
     </Col>
     <Col sm={4}>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" name="email" />
     </Col>
     <Button variant="primary" type="submit">
     Submit
@@ -45,5 +48,19 @@ const footer = (props) => (
 </span>
     </div>
 );
+
+function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_g5wg4n6', 'template_o3itg1m', e.target, 'user_y0y03N8lgIFynSKHRp1s5')
+      .then((result) => {
+          console.log(result.text);
+          alert("Email sent!");
+      }, (error) => {
+          console.log(error.text);
+          alert("Something went wrong " + error.text);
+      });
+      e.target.reset()
+  }
 
 export default footer;
