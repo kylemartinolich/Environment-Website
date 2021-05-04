e48cc00610499e40e270d1a4bddaf49915afabf8
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-04 21:47:53 +0000

    trying out svg again

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -45,9 +45,12 @@ const footer = (props) => (
   </Button>
   </Form.Row>
 </Form>
-
-    <Image src="../../assets/Miscellaneous/fbicon.png" fluid />
-
+<div>
+<svg aria-hidden="true" data-prefix="fab" fill="black" data-icon="facebook-f" width="16" height="16"
+    class="svg-inline--fa fa-facebook-f fa-w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/>
+</svg>
+</div>
 <span>
     © 2021 
 </span>
