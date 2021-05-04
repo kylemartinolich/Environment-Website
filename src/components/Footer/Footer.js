4c575512def23138fda1db24a7a09a89c2e025ea
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-04 20:51:02 +0000

    turn svg to image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 import emailjs from 'emailjs-com';
 import Toast from 'react-bootstrap/Toast';
 import classes from './Footer.module.css';
+import fbicon from '../../assets/Miscellaneous/facebook-f-brands.svg';
 
 const footer = (props) => (
     <div className={classes.Footer}>
@@ -43,10 +44,7 @@ const footer = (props) => (
   </Button>
   </Form.Row>
 </Form>
-<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" 
-class="svg-inline--fa fa-facebook-f fa-w-10" role="img" 
-xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
-    <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>
+<img src="fbicon" alt="facebook icon"></img>
 <span>
     © 2021 
 </span>
