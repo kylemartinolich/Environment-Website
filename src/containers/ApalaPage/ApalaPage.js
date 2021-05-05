10a63ed578fa80934e4f63c49b1706b525ec20f1
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-05 20:19:47 +0000

    add more to background image

--- src/containers/ApalaPage/ApalaPage.js
+++ src/containers/ApalaPage/ApalaPage.js
@@ -13,6 +13,8 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import apapic from '../../assets/Rivers/apalachicola.jpg';
 import ImageMapper from 'react-image-mapper';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const renderTooltip = (props) => (
   <Tooltip id="button-tooltip" {...props}>
@@ -104,8 +106,13 @@ let MAP = {
             <div className={classes.chevronContainer}><i onClick={() =>window.scrollTo(100,750)}className={classes.chevron}></i></div>
                 <div className={classes.text}>
                     <div className={classes.info}>
-                        <h2>Welcome to the Apalachicola!</h2>
-                        <p>
+                        <Row>
+                            <Col md="auto"><h2 className = {classes.h2}>Welcome to the Apalachicola!</h2></Col>
+                            <Col></Col>
+                        </Row>
+                        <Row>
+                            <Col md="8">
+                        <p className={classes.pL}>
                             The Apalachicola River basin is a geographic area in the Florida Panhandle that drains a watershed of some
                             20,000 square miles and it is one of the nations most important biological hotspots. The headwaters of the
                             Apalachicola are the Chattahoochee River flowing from Georgia’s southern Appalachians, and the Flint River,
@@ -113,25 +120,43 @@ let MAP = {
                             107 miles south to Apalachicola Bay. It rolls past 150-foot tall bluffs, etched with deep steephead ravines,
                             and the state’s largest floodplain forest – great habitat for all kinds of animals.
                         </p>
+                        </Col>
+                        <Col></Col>
+                        </Row>
                         <br />
-                        <p>
+                        <Row>
+                            <Col></Col>
+                            <Col md="8">
+                        <p className={classes.pR}>
                             The Apalachicola River basin has the highest density of amphibian (44) and reptile (64) species north of Mexico.
                             The river and its floodplain are also home to more than 1,000 species of plants, 52 species of mammals, 60
                             species of snails and clams, five federally listed mussels, 315 bird species, and 86 species of fish.
                         </p>
+                         </Col>
+                        </Row>
                         <br />
-                        <p>
+                         <Row>
+                            <Col md="8">
+                        <p className={classes.pL}>
                             Historically, Apalachicola Bay produced 90 percent of Florida’s oysters and 13 percent of the nation’s harvest.
                             It was known for its outstanding commercial fishing, shrimping and crabbing. Unfortunately the bay’s productively
                             declined as the river was manipulated over the years.
                         </p>
+                        </Col>
+                        <Col></Col>
+                        </Row>
                         <br />
-                        <p>
+                        <Row>
+                            <Col></Col>
+                            <Col md="8">
+                        <p className={classes.pR}>
                             In 2010 this outstanding fishery collapsed due to little freshwater flowing into the estuary and over-harvesting
                             in response to the Deepwater Horizon oil spill. The bay’s oyster populations have plummeted, and some of its underwater
                             plants have disappeared as a result which has impacted many of the bay’s other species. The bay needs thriving oyster
                             reefs and aquatic vegetation to provide cover and breeding ground for mussels, crabs, snails, shrimp and fish.
                         </p>
+                         </Col>
+                        </Row>
                     </div>
                     <br></br>
                     <AnimalButton show={this.state.modalShow} onHide={() => this.setState({modalShow:false})} />
