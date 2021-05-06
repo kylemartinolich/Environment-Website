54cbd9fb695cecc5fefd3a2716a170e13b53bf53
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-06 19:39:29 +0000

    stylings for modal

--- src/components/AnimalButton/AnimalButton.js
+++ src/components/AnimalButton/AnimalButton.js
@@ -3,23 +3,23 @@ import classes from './AnimalButton.module.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 const animalButton = (props) => (<div>
-    <Modal
+    <Modal className={classes.text}
       {...props}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
+      <Modal.Header className={classes.heading} closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           {props.animal}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <p className={classes.p}>
-          {props.description}
-        </p>
+      <Modal.Body className={classes.p}>
+                    <p>
+                        {props.description}
+                        </p>
       </Modal.Body>
-      <Modal.Footer>
+      <Modal.Footer className={classes.border}>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
