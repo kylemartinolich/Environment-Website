8b83bb9b690c0b172440cdfd98a64446b427f61b
Kdmman2 <kdmman2@gmail.com>
2021-05-06 22:19:31 +0000

    more cleanup

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -3,20 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingButton from '../../components/LandingButton/LandingButton';
 import Chattahoochee_watershed from '../../assets/Chattahoochee_watershed.png';
 import classes from './LandingPage.module.css';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
 
 var ReactRotatingText = require('react-rotating-text');
-const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-        Chattahoochee River, Georgia © Keizers
-    </Tooltip>
-);
-const mapTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-        ACF Projection © Chattahoochee River Keeper
-    </Tooltip>
-);
 class LandingPage extends Component {
     state = {
         image: Chattahoochee_watershed,
@@ -42,32 +30,8 @@ class LandingPage extends Component {
                             <div className={classes.chevronContainer}><i onClick={() => window.scrollTo(100, 850)} className={classes.chevron}></i></div>
                         </div>
                     </div>
-                    <div class="position-absolute bottom-0 left-0">
-                        <OverlayTrigger
-                            placement="right"
-                            delay={{ show: 250, hide: 400 }}
-                            overlay={renderTooltip}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                            </svg>
-                        </OverlayTrigger>
-                    </div>
                 </header>
                 <div className={classes.mapContainer}>
-                    <div class="position-absolute bottom-0 left-0">
-                        <OverlayTrigger
-                            placement="right"
-                            delay={{ show: 250, hide: 400 }}
-                            overlay={mapTooltip}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                            </svg>
-                        </OverlayTrigger>
-                    </div>
                     <div className={classes.text}>
                         <h2>Welcome to the Environment Website!</h2>
                         <p>
