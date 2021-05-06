47ab438b4ec40b5d24da09f5734c3adb71356859
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-06 16:43:07 +0000

    testing modals to style

--- src/components/AnimalButton/AnimalButton.js
+++ src/components/AnimalButton/AnimalButton.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import classes from './AnimalButton.module.css';
-import Plus from '../../assets/Miscellaneous/plus.png';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-var modal;
 const animalButton = (props) => (<div>
     <Modal
       {...props}
@@ -16,15 +14,12 @@ const animalButton = (props) => (<div>
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          {props.animal}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+        <p className={classes.p}>
+          {props.description}
         </p>
       </Modal.Body>
       <Modal.Footer>
@@ -34,7 +29,4 @@ const animalButton = (props) => (<div>
     </div>
     
 );
-
 export default animalButton;
-
-//<div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- src/containers/ApalaPage/ApalaPage.js
+++ src/containers/ApalaPage/ApalaPage.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Button from './components/Button/Button';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import classes from './ApalaPage.module.css';
 import AccordionPage from '../../components/Accordion/Accordion';
 import SpeciesData from '../../speciesdata.json';
 import AnimalButton from '../../components/AnimalButton/AnimalButton';
-import Plus from '../../assets/Miscellaneous/plus.png';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import apapic from '../../assets/Rivers/apalachicola.jpg';
@@ -22,52 +18,37 @@ const renderTooltip = (props) => (
   </Tooltip>
 );
 class ApalaPage extends Component{
-
     constructor(props) {
         super(props);
         this.state = { windowWidth: window.innerWidth };
     }
-
+    state={
+        activeAnimal: null,
+        modalShow: false
+    }
     handleResize = (e) => {
         this.setState({ windowWidth: window.innerWidth });
-    };
-
+    }
     componentDidMount() {
         window.addEventListener("resize", this.handleResize);
     }
-
     componentWillUnmount() {
         window.addEventListener("resize", this.handleResize);
     }
-
-
-
-    state={
-        modalShow: false
-    }
-
     showModalHandler = (handle)=>{
-        this.setState({showModal: !this.state.showModal});
-        console.log("Test");
+        this.setState({activeAnimal: handle, modalShow: !this.state.modalShow});
     }
-
     enterArea(area) {
     this.setState({ hoveredArea: area });
     }
-
     leaveArea(area) {
         this.setState({ hoveredArea: null });
     }
-
     getTipPosition(area) {
         return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
     }
-
     render(){
-
         const { windowWidth } = this.state;
-        /*return <div>Current window width: {windowWidth}</div>*/
-
         let URL = {apapic}
 let MAP = {
   name: "my-map",
@@ -75,20 +56,24 @@ let MAP = {
     { name: "Eastern Indigo Snake", shape: "circle", coords: [2277,667,172]},
     { name: "Torreya Tree", shape: "circle", coords: [435,655,170]},
     { name: "Gulf Sturgeon", shape: "circle", coords: [1672,961,179]},
-    { name: "Red Cockaded Woodpecker", shape: "circle", coords: [1672,298,173]},
+    { name: "Red-cockaded Woodpecker", shape: "circle", coords: [1672,298,173]},
     { name: "Fat Threeridge Mussel", shape: "circle", coords: [2456,1169,167]},
     { name: "Apalachicola Dusky Salamander", shape: "circle", coords: [2932,655,167]},
     { name: "Florida Black Bear", shape: "circle", coords: [1014,655,170]},
-    { name: "Oysters", shape: "circle", coords: [888,1137,171]}
-    
+    { name: "Oysters (Crassostrea virginica)", shape: "circle", coords: [888,1137,171]} 
   ]
 }
-
+var description;
+for(let i = 0; i < SpeciesData.apa.length; i++){
+    if(this.state.activeAnimal === SpeciesData.apa[i].name){
+        description = SpeciesData.apa[i].info;
+    }
+}
         return(
         <div>
             <div className="showcase">
             <ImageMapper src={apapic} map={MAP} width={windowWidth} imgWidth={3260}
-    	        onClick={area => this.clicked(area)}
+    	        onClick={area => this.showModalHandler(area.name)}
     	        onMouseEnter={area => this.enterArea(area)}
     	        onMouseLeave={area => this.leaveArea(area)}
                 />
@@ -104,8 +89,8 @@ let MAP = {
             <AccordionPage animalList={SpeciesData.apa}/>
             </div>
             <div className={classes.chevronContainer}><i onClick={() =>window.scrollTo(100,750)}className={classes.chevron}></i></div>
-                <div className={classes.text}>
-                    <div className={classes.info}>
+            <div className={classes.text}>
+                 <div className={classes.info}>
                         <Row>
                             <Col md="auto"><h2 className = {classes.h2}>Welcome to the Apalachicola!</h2></Col>
                             <Col></Col>
@@ -158,47 +143,10 @@ let MAP = {
                          </Col>
                         </Row>
                     </div>
-                    <br></br>
-                    <AnimalButton show={this.state.modalShow} onHide={() => this.setState({modalShow:false})} />
+            <br></br>
+            <AnimalButton show={this.state.modalShow} animal={this.state.activeAnimal} description={description} onHide={() => this.setState({modalShow:false})} />
             </div>
 </div>);
     }
 }
 export default ApalaPage;
-/*
-<Jumbotron>
-  <h1>Hello, Apalachacola!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>
-*/
-
-/*
-        <header className={classes.showcase}>
-                <div className={classes.showcasecontent}>
-                    <img  className={classes.plusButton} style={{top: '520px', right: '230px'}} onClick={() => this.setState({modalShow:true})} src={Plus} alt="Plus Button"></img>
-                    <img  className={classes.plusButton} style={{top: '120px', left: '230px'}} onClick={() => this.setState({modalShow:true})} src={Plus} alt="Plus Button"></img>
-                    <div className={classes.container}>
-                        <div className={classes.chevronContainer}><i onClick={() =>window.scrollTo(100,1000)}className={classes.chevron}></i></div>
-                    </div>
-                </div>
-            <div class="position-absolute bottom-0 left-0">
-                    <OverlayTrigger
-                        placement="right"
-                            delay={{ show: 250, hide: 400 }}
-                                overlay={renderTooltip}
-                                >
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                    </svg>
-                                                                  
-                    </OverlayTrigger>
-                </div>
-            </header>
-*/
