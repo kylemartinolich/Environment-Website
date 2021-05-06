6f12dd32a7d5f8aabb7030e239ff27fa77fa5b97
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2021-05-06 05:05:16 +0000

    resize jumbotron

--- src/containers/LowerChattaPage/LowerChattaPage.js
+++ src/containers/LowerChattaPage/LowerChattaPage.js
@@ -6,6 +6,7 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import jumbopic from '../../assets/Rivers/lakelanier.jpg'
 
 
 const renderTooltip = (props) => (
@@ -15,35 +16,35 @@ const renderTooltip = (props) => (
 );
 
 class LowerChattaPage extends Component{
+constructor(props) {
+        super(props);
+        this.state = { windowWidth: window.innerWidth };
+    }
 
+    handleResize = (e) => {
+        this.setState({ windowWidth: window.innerWidth });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("resize", this.handleResize);
+    }
     render(){
+        const { windowWidth } = this.state;
         return(
-
         <div>
+            <Row>
+             <Jumbotron>
                     <div>
-            <jumbotron fluid>
-                
-            </jumbotron>
-        </div>
-            <header className={classes.showcase}>
-                <div className={classes.showcasecontent}>
-                    <div className={classes.container}>
+                         <img src={jumbopic} width={windowWidth} imgWidth={1920}/>
                     </div>
+                </Jumbotron>
+                </Row>
+                <div className={classes.showcasecontent}>
                 </div>
-                <div class="position-absolute bottom-0 left-0">
-                    <OverlayTrigger
-                        placement="right"
-                            delay={{ show: 250, hide: 400 }}
-                                overlay={renderTooltip}
-                                >
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                    </svg>
-                                                                  
-                    </OverlayTrigger>
-                </div>
-            </header>
             <div className={classes.text}>
             <div className={classes.info}>
                         <Row>
